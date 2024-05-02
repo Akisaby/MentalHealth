48f9fddbd95a5efc200a13360ab68289e7de1317
tflorence4 <tumusabeflomu@gmail.com>
2024-05-02 20:33:50 +0200

    commit

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('', views.article_list, name='rticle_list'),  # Example view
+ 
+    path('', views.article_list, name='article_list'),  # Example view
     path('article/<int:pk>/', views.article_detail, name='article_detail'),
     #path('article/<int:pk>/', views.article_detail, name='article_detail'),  # Example detail view
     # Add more URL patterns for your views here
+
+
+    path('login/', views.login_view, name='login'),
+
+    
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,6 @@
 
 from django.shortcuts import render, get_object_or_404
 from .models import Article
-
 def article_list(request):
     articles = Article.objects.all()
     return render(request, 'article/article_list.html',{'articles': articles})
@@ -11,3 +10,7 @@ def article_detail(request, pk):
     articles = Article.objects.all()
     context ={'articles': articles,'article': article}
     return render(request, 'article/article_detail.html',context)
+
+
+def login_view(request):
+    return render(request, 'login.html')
